71cad99b8e579dfb0eb77c7d92df6ece5cce5f3d
Fabis-li <fabiano_slima@hotmail.com>
2024-07-04 15:45:15 -0300

    criado diagrama

---
+++ ControleDeBar.Dominio/Conta.cs
@@ -0,0 +1,29 @@
+﻿namespace ControleDeBar.Dominio
+{
+    public class Conta
+    {
+        public int Pedido
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Mesa
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int ValorTotal
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
