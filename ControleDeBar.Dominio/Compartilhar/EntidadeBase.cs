88309af17cbdcbfa6e0a5d105dc1df490c6f4f08
Fabis-li <fabiano_slima@hotmail.com>
2024-07-05 13:43:57 -0300

    criado classe produto e entidade base

---
+++ ControleDeBar.Dominio/Compartilhar/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleDeBar.Dominio.Compartilhar
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; set; }
+
+        public abstract List<string> Validar();
+
+        public abstract void AtualizarRegistro(EntidadeBase novoRegistro);
+    }
+}
---
+++ ControleDeBar.Dominio/ModuloProduto/Produto.cs
@@ -0,0 +1,56 @@
+﻿using ControleDeBar.Dominio.Compartilhar;
+
+namespace ControleDeBar.Dominio.ModuloProduto
+{
+    public class Produto : EntidadeBase
+    {
+        public string Nome { get; set;}        
+
+        public int Valor { get; set; }
+
+        public int Qtde { get; set; }
+
+        public Produto() { }
+
+        public Produto(string nome, int valor, int qtde)
+        {
+            Nome = nome;
+            Valor = valor;
+            Qtde = qtde;
+        }
+
+        public override List<string> Validar()
+        {
+            List<string> erros = new List<string>();
+
+            if(string.IsNullOrEmpty(Nome))
+                erros.Add("Nome do produto é obrigatório");
+
+            if(Valor < 1)
+                erros.Add("Valor do produto deve ser maior do zero");
+
+            if(Qtde < 1)
+                erros.Add("Quantidade do produto deve ser maior do zero");
+
+            return erros;
+        }
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoverProduto(int qtde)
+        {
+            Qtde -= qtde;
+        }
+        public void AdicionarProduto(int qtde)
+        {
+            Qtde += qtde;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Valor: {Valor}, Qtde: {Qtde}";
+        }
+    }
+}
