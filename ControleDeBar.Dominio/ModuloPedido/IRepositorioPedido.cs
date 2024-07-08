d9c4792ef90f6392b239d1737cf1cd6c1ce05449
Fabis-li <fabiano_slima@hotmail.com>
2024-07-07 23:21:54 -0300

    criado interface do repositorio

---
+++ ControleDeBar.Dominio/ModuloPedido/IRepositorioPedido.cs
@@ -0,0 +1,13 @@
+﻿namespace ControleDeBar.Dominio.ModuloPedido
+{
+    public interface IRepositorioPedido
+    {
+        void Adicionar(Pedido pedido);
+        bool Editar(int id, Pedido pedido);
+        bool Excluir(int id);
+
+        Pedido SelecionarPorId(int id);
+        List<Pedido> SelecionarTodos();
+    }
+}
+
