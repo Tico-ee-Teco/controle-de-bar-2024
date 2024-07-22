b3b431f26f411e677651ad6df02873abe8f0abcc
Fabis-li <fabiano_slima@hotmail.com>
2024-07-22 20:18:28 -0300

    teste webapp

--- ControleDeBar.WebApp/Program.cs
+++ ControleDeBar.WebApp/Program.cs
@@ -1,3 +1,7 @@
+using ControleDeBar.Dominio.ModuloMesa;
+using ControleDeBar.Infra.Compartilhado;
+using ControleDeBar.Infra.ModuloMesa;
+
 namespace ControleDeBar.WebApp
 {
     public class Program
@@ -7,9 +11,24 @@ namespace ControleDeBar.WebApp
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", OlaMundo);
+
+            app.MapGet("/mesas/listar", ListarMesas);
 
             app.Run();
+        }
+
+        private static async Task ListarMesas(HttpContext context)
+        {
+            ControleDeBarDbContext db =  new ControleDeBarDbContext();
+            IRepositorioMesa repositorioMesa = new RepositorioMesa(db);
+        }
+
+        private static Task OlaMundo(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain; charset=utf-8 ";   
+
+             return context.Response.WriteAsync("Olá Mundo!");
         }
     }
 }
