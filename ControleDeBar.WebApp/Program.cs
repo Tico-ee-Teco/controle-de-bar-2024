734e41906094960555edc965d4ae0a0a2b7d783b
Fabis-li <fabiano_slima@hotmail.com>
2024-07-22 19:24:47 -0300

    criado webapp

--- ControleDeBar.WebApp/Program.cs
+++ ControleDeBar.WebApp/Program.cs
@@ -1,36 +1,15 @@
-namespace ControleDeBar.WebApp;
-
-public class Program
+namespace ControleDeBar.WebApp
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
 
-        app.MapGet("/", OlaMundo);
+            app.MapGet("/", () => "Hello World!");
 
-        app.MapGet("/mesas/listar", ListarMesas);
-
-        app.Run();
-    }
-
-    private static Task ListarMesas(HttpContext context)
-    {
-        ControleDeBarDbContext db = new ControleDeBarDbContext();
-        IRepositorioMesa repositorioMesa = new IRepositorioMesa(db);
-
-        IEnumerable<string> mesasStrings = repositorioMesa
-            .SelecionarTodos()
-            .Select(mesa => "-> " + mesa.ToString());
-
-        string resposta = string.Join('\n', mesasStrings);  
-        context.Response.ContentType = "text/plain; charset=utf-8";
-        return context.Response.WriteAsync(resposta);
-    }
-
-    private static Task OlaMundo(HttpContext context)
-    {
-        context.Response.ContentType = "text/plain; charset=utf-8";
-        return context.Response.WriteAsync("Olá, mundo!");
+            app.Run();
+        }
     }
 }
