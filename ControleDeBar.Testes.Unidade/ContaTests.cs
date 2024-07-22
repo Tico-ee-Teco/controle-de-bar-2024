a27ed1bb9e9afb9b225e0018e3f3b883a75d4828
Fabis-li <fabiano_slima@hotmail.com>
2024-07-22 17:09:20 -0300

    tests

---
+++ ControleDeBar.Testes.Unidade/ContaTests.cs
@@ -0,0 +1,11 @@
+namespace ControleDeBar.Testes.Unidade
+{
+    [TestClass]
+    public class ContaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
