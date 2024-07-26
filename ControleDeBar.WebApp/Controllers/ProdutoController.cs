b2f167497e3da3fc23528ef7b7574895edddc104
Fabis-li <fabiano_slima@hotmail.com>
2024-07-26 09:53:35 -0300

    criado view lista produto

---
+++ ControleDeBar.WebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using ControleDeBar.Infra.Compartilhado;
+using ControleDeBar.Infra.ModuloProduto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeBar.WebApp.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Listar()
+        {
+            var dbContext = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProduto(dbContext);
+
+            var produtos = repositorioProduto.SelecionarTodos();
+
+            ViewBag.Produtos = produtos;
+
+            return View();
+        }
+        
+    }
+}
